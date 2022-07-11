ffb07460ea28e23d87d6141a2aeb67957bd4a558
Dimitri Leduc <dimitrileduc@MacBook-Pro-de-Dimitri.local>
2022-07-11 14:58:12 +0200

    footer grid layout

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,6 +1,24 @@
 <template>
     <div class="box">
-        <div class="container">Footer</div>
+        <div class="container">
+            <div class="button">{{ buttonTitle }}</div>
+            <div class="menu1">
+                <ul>
+                    <div class="titleMenu">{{ titleMenu1 }}</div>
+                    <li v-for="item in itemsDetailsMenu1" :key="item">
+                        {{ item }}
+                    </li>
+                </ul>
+            </div>
+            <div class="menu2">
+                <ul>
+                    <div class="titleMenu">{{ titleMenuContact }}</div>
+                    <li v-for="item in itemsContactMenu" :key="item">
+                        {{ item }}
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -13,7 +31,25 @@ export default {
     name: "FooterSection",
     components: {},
     data() {
-        return {};
+        return {
+            buttonTitle: "book",
+            titleMenu1: "villa",
+            itemsDetailsMenu1: [
+                "villa",
+                "rooms",
+                "activities",
+                "calendar",
+                "rules",
+            ],
+            titleMenuContact: "social",
+            itemsContactMenu: [
+                "hello@unico.com",
+                "facebook",
+                "whatsapp",
+                "instagram",
+                "chat",
+            ],
+        };
     },
     mounted() {},
 };
@@ -22,13 +58,92 @@ export default {
 <style scoped>
 .box {
     width: 100vw;
-    height: 30vh;
-    background-color: grey;
+
     z-index: 500;
-    margin-top: 0vh;
+
+    position: absolute;
+    bottom: 0;
+    margin-bottom: 60px;
 }
 .container {
-    font-size: 112px;
-    color: white;
+    margin-top: auto;
+    display: grid;
+    grid-template-columns: 2fr 1fr 1fr;
+    grid-template-rows: 1fr;
+    gap: 0px 0px;
+    grid-auto-flow: row;
+    grid-template-areas: "button menu1 menu2";
+    border: 1px red dashed;
+
+    margin-left: 40px;
+    margin-bottom: 40px;
+    margin-right: 40px;
+}
+
+.button {
+    justify-self: start;
+    align-self: end;
+    grid-area: button;
+    margin-bottom: 40px;
+    font-family: "Karla";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 48px;
+    letter-spacing: 0.05em;
+    text-decoration-line: underline;
+
+    color: #000000;
+}
+
+.menu1 {
+    justify-self: start;
+    align-self: end;
+    grid-area: menu1;
+    text-align: left;
+    font-family: "Lato";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 24px;
+    /* or 156% */
+
+    letter-spacing: 0.05em;
+
+    color: #000000;
+}
+
+ul {
+    list-style-type: none;
+}
+
+.menu2 {
+    justify-self: start;
+    align-self: end;
+    grid-area: menu2;
+    text-align: left;
+    font-family: "Lato";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 24px;
+    /* or 156% */
+
+    letter-spacing: 0.05em;
+
+    color: #000000;
+}
+
+.titleMenu {
+    font-family: "Karla";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 21px;
+    /* identical to box height */
+
+    letter-spacing: 0.05em;
+
+    color: #000000;
+    margin-bottom: 20px;
 }
 </style>
--- src/components/HomeDetails.vue
+++ src/components/HomeDetails.vue
@@ -1,6 +1,22 @@
 <template>
     <div class="container">
-        <h3>{{ dataProps.sectionName }} section</h3>
+        <div class="Title">{{ title }}</div>
+        <div class="details">
+            <div class="detailsTitle">{{ titleDetails }}</div>
+            <ul>
+                <li v-for="item in itemsDetails" :key="item">
+                    {{ item }}
+                </li>
+            </ul>
+        </div>
+        <div class="containerPics">
+            <div class="pic1">Pic 1</div>
+            <div class="pic2">Pic 2</div>
+        </div>
+        <div class="description">
+            <div class="description1">{{ description1 }}</div>
+            <div class="description2">{{ description2 }}</div>
+        </div>
     </div>
 </template>
 
@@ -8,7 +24,24 @@
 export default {
     name: "HomeDetails",
     data() {
-        return {};
+        return {
+            title: "Unico is more than a luxury stay. It’s a space designed for opening the mind",
+            titleDetails: "Villa details",
+            itemsDetails: [
+                "Living Room",
+                "The Ktichen",
+                "Bedrooms",
+                "Pool Area",
+                "Spa",
+                "Rooftop",
+                "Garden",
+                "Pool",
+            ],
+            description1:
+                "The intention behind Bond was to always bring people together, whilst respecting diverse group dynamics and personal space. The owners and architect Alexis Dornier wanted to create a private & intimate space that still felt open and connected to the world and nature around them. A hub created for all forms of play, free from any restrictions or limitations.",
+            description2:
+                "When you visit, you can feel the natural energy of Bond’s surroundings helping you reground, recharge and regrow. This expansion leads to new ideas, collaborations, and most and foremost, a pretty good & healing time for those joining you.",
+        };
     },
     props: {
         dataProps: {
@@ -29,5 +62,119 @@ export default {
     height: 100vh;
     position: relative;
     z-index: 10;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    gap: 0px 0px;
+    grid-auto-flow: row;
+    grid-template-areas:
+        "Title Title"
+        "details description"
+        "containerPics .";
+}
+
+.Title {
+    justify-self: start;
+    grid-area: Title;
+    margin-top: 40px;
+    margin-left: 40px;
+    margin-right: 160px;
+
+    position: absolute;
+
+    font-family: "Lato";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 36px;
+    line-height: 55px;
+    letter-spacing: 0.04em;
+    text-align: left;
+
+    color: #000000;
+    text-transform: uppercase;
+}
+
+ul {
+    list-style-type: none;
+}
+
+.details {
+    margin-top: 0px;
+    justify-self: start;
+    align-self: start;
+    grid-area: details;
+
+    margin-left: 80px;
+    text-align: left;
+    font-family: "Lato";
+    font-style: italic;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 25px;
+    /* or 139% */
+
+    letter-spacing: 0.05em;
+
+    color: #000000;
+    margin-bottom: 40px;
+}
+
+.detailsTitle {
+    font-family: "Lato";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 25px;
+    text-transform: uppercase;
+
+    letter-spacing: 0.05em;
+
+    color: #000000;
+    margin-bottom: 20px;
+}
+
+.containerPics {
+    justify-self: start;
+    align-self: end;
+    grid-area: containerPics;
+    border: 1px red dashed;
+    margin-left: 40px;
+    margin-bottom: 40px;
+    display: flex;
+}
+
+.pic1 {
+    width: 20vh;
+    height: 30vh;
+    border: 1px grey dashed;
+}
+.pic2 {
+    margin-left: 20px;
+    width: 40vh;
+    height: 30vh;
+    border: 1px grey dashed;
+}
+
+.description {
+    margin-top: 40px;
+    justify-self: end;
+    grid-area: description;
+
+    margin-right: 80px;
+    font-family: "Karla";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 20px;
+    /* or 144% */
+
+    letter-spacing: 0.05em;
+
+    color: #000000;
+    text-align: left;
+}
+
+.description2 {
+    margin-top: 20px;
 }
 </style>
